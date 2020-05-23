c0ffe5ce6fe736f49dd05d7725acc9d43f792142
Princess Lana <ianagbip1oti@gmail.com>
2020-05-23 12:44:18 -0700

    🐛 FIX: missing import

--- src/main/java/com/github/princesslana/showme/ShowMe.java
+++ src/main/java/com/github/princesslana/showme/ShowMe.java
@@ -17,6 +17,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Consumer;
