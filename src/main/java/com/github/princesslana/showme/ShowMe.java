f1dcccf440fdd2033d216d238f630240e4224508
ianagbip1oti <ianagbip1oti@gmail.com>
2020-05-23 19:01:52 +0000

    📦 NEW: Allow restrict to single channel

--- src/main/java/com/github/princesslana/showme/ShowMe.java
+++ src/main/java/com/github/princesslana/showme/ShowMe.java
@@ -3,6 +3,11 @@ package com.github.princesslana.showme;
 import com.github.princesslana.smalld.Attachment;
 import com.github.princesslana.smalld.SmallD;
 import com.google.gson.JsonObject;
+import disparse.discord.smalld.SmallDDispatcher;
+import disparse.discord.smalld.SmallDEvent;
+import disparse.discord.smalld.SmallDUtils;
+import disparse.parser.reflection.CommandHandler;
+import disparse.parser.reflection.ParsedEntity;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
@@ -12,15 +17,18 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
-import disparse.discord.smalld.SmallDDispatcher;
-import disparse.discord.smalld.SmallDEvent;
-import disparse.discord.smalld.SmallDUtils;
-import disparse.parser.reflection.CommandHandler;
-import disparse.parser.reflection.ParsedEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ShowMe implements Consumer<SmallD> {
 
+  private static final Logger LOG = LoggerFactory.getLogger(ShowMe.class);
+
+  /** Path to data folder. Categories should be subfolders wlithin here */
   private static final Path DATA_PATH = Paths.get(System.getenv("SHOWME_DATA"));
+
+  /** Optionally restrict to the given channel id */
+  private static final String CHANNEL_ID = System.getenv("SHOWME_CHANNEL_ID");
 
   @CommandHandler(
     commandName = "showme",
@@ -34,14 +42,33 @@ public class ShowMe implements Consumer<SmallD> {
     }
 
     String ofWhat = args.get(0);
+
+    // Let's only accept alphanumeric to prevent funky stuff like / or .. here
+    if (!ofWhat.matches("^[a-zA-Z0-9]+$")) {
+      LOG.warn("Invalid request for '" + ofWhat + "' pics");
+      return;
+      
+    }
+
     String channelId = SmallDUtils.getChannelId(evt.getJson());
 
+    if (CHANNEL_ID != null && !CHANNEL_ID.equals(channelId)) {
+      LOG.warn("Request was in disallowed channel: " + channelId);
+      return;
+    }
+
     Path folder = DATA_PATH.resolve(ofWhat);
+
+    if (!Files.isDirectory(folder)) {
+      LOG.warn("Request for images that don't exist: " + ofWhat);
+      return
+    }
 
     List<Path> images =
       Files.list(folder).filter(Files::isRegularFile).collect(Collectors.toList());
 
-    Attachment attachment = new Attachment(ofWhat + ".jpg", "image/jpeg", randomChoice(images).toUri().toURL());
+    Attachment attachment = new Attachment(
+        ofWhat + ".jpg", "image/jpeg", randomChoice(images).toUri().toURL());
 
     evt.getSmalld().post(
       "/channels/" + channelId + "/messages",
@@ -65,11 +92,12 @@ public class ShowMe implements Consumer<SmallD> {
   }
 
   public static final void main(String[] args) {
+    if (!Files.isDirectory(DATA_PATH)) {
+      throw new IllegalStateException("Data path does not point to a folder");
+    }
+
     SmallD.run(System.getenv("SHOWME_TOKEN"), new ShowMe());
   }
-
-
-
 
 }
 
